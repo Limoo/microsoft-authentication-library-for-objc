e609fb9c8800afc77b2f7c99559abe4b754b5c92
Tibor Leon Hahne <mixer_11@hotmail.de>
2020-11-13 15:43:23 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSAL",
-            url: "https://github.com/mitchdenny/microsoft-authentication-library-for-objc/releases/download/1.1.6-beta.1/MSAL.xcframework.zip",
+            url: "https://github.com/Limoo/microsoft-authentication-library-for-objc/releases/download/1.1.6-beta.1/MSAL.xcframework.zip",
             checksum: "57cca1fa07c9bf0ce4e0c7e367a7530cdb08bb8399cd00ff7cf7b8f12f45849d"
             )
     ]
